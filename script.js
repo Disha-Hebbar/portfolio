9af7623feeb9bca0f13019e3f66430a18770673d
Disha-Hebbar <109352031+Disha-Hebbar@users.noreply.github.com>
2025-09-01 12:59:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function toggleMenu(){
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
-} 
+// Cursor-following dot
+const cursorDot = document.querySelector(".cursor-dot");
+
+document.addEventListener("mousemove", (e) => {
+  cursorDot.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+});
